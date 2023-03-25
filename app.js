66b9e13cefe952637022f4026cd36b5156d2f639
minhaz000 <mrmminhaz@gamail.com>
2023-03-25 22:08:54 +0600

    cors issue solved

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 require('colors')
 const express = require('express')
+const  cors = require('cors')
 const Router = require('./routes/root.routes')
 const ApiRouter = require('./routes/API/v1/api.routes')
 const DB = require('./db.config')
@@ -9,6 +10,7 @@ DB.connectDB
 
 app.use('/static',express.static('public'))
 app.use(express.json());
+app.use(cors())
 app.use('/',Router);
 app.use('/api',ApiRouter);
 
